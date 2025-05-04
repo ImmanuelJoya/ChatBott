4e86dc411d45512a40e21403c094f07f119c8d36
Immanuel Joy <immanueljoy107@gmail.con>
2025-05-03 23:40:58 -0400

    added user registeration

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import cors from "cors"
 import dotenv from "dotenv"
 import express from "express"
+import { StreamChat } from "stream-chat"
 
 dotenv.config()
 
@@ -10,8 +11,39 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+
+//Initilize stream client
+const chatClient = StreamChat.getInstance(
+    process.env.STREAM_API_KEY!,  //"!" telling ts that we are sure it has a value, that its not gonna be null or undefined
+    process.env.STREAM_API_SECRET!
+)
+
+//Register user with stream chat 
+app.post('/register-user', async (req: Request, res: Response): Promise<any> => {
+    const { name, email } = req.body
+
+    if (!name || !email) {
+        return res.status(400).json({ error: 'Missing name or email' })
+    }
+
+    try {
+
+        const userId = email.replace(/[^a-zA-Z0-9]/g, '_') //replace special characters with underscore
+        console.log(userId);
+
+        res.status(200).json({ message: 'User registered successfully' })
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Internal server error' })
+    }
+
+
+
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
+    console.log(`http://localhost:${PORT}`);
 })
